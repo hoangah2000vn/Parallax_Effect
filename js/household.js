990add5998c7a0a1334073793b5b835b3ab38715
Rohit <rohitsanatani@yahoo.co.in>
2023-05-14 19:03:11 -0400

    Rohit commit Sunday

--- js/household.js
+++ js/household.js
@@ -1,3 +1,20 @@
+
+function set_object(thisObject){
+    //console.log(thisObject);
+    objects = ['tv','fridge','microwave','stove','computer','internet','telephone','ac','car','washer','stereo'];
+    for (i = 0;i <objects.length;i++){
+        print(objects[i])
+        objectDiv = document.getElementById(objects[i]);
+        //console.log(objectDiv);
+        objectDiv.style.display = 'none';
+    }
+    objectDiv = document.getElementById(thisObject);
+    objectDiv.style.display = 'block';
+}
+
+
+
+/*
 const tvmodal = document.querySelectorAll('.tvmodal')[0]
 const openTvButton = document.querySelectorAll('.tv_open')[0]
 const closeTvButton = document.querySelectorAll('.tv_close')[0]
@@ -11,6 +28,10 @@ closeTvButton.addEventListener('click', () => {
     tvmodal.style.display = 'none';
     console.log('closing')
 })
+*/
+
+
+
 
 
 
